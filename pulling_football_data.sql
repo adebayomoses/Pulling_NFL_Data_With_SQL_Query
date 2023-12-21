0efe918e5af9e91a700e1cbb1998c99cedce4f70
Adebayo Moses <109078105+skuadsco@users.noreply.github.com>
2023-12-21 00:37:47 -0500

    Add files via upload

--- pulling_football_data.sql
+++ pulling_football_data.sql
@@ -1,5 +1,14 @@
 
 SELECT *
 
-FROM Tables
+FROM Country AS c
+JOIN League AS l ON c.id = l.id
+JOIN Match AS m ON l.country_id = m.country_id
+JOIN Player AS p ON m.id = p.id
+JOIN Player_Attributes AS a ON  p.player_api_id = a.player_api_id
+JOIN Team AS t ON a.id = t.id
+JOIN Team_Attributes AS att ON t.team_api_id = att.team_api_id
+
+
+LIMIT 5
 
